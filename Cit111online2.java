fc38e8b8fcd12dcfe28db8cdd1c4d1d7a2199003
Eric Darsow <edarsow@ccac.edu>
2020-10-12 09:32:23 -0400

    Completed Might We Be Friends? project

---
+++ Cit111online2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit111online2;
+
+/**
+ * Class containing my practice for this week in Java online @ ccac
+ * @author edith
+ */
+public class Cit111online2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Work dammit!");
+    }
+    
+}
